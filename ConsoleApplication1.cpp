a09b183e5cd187f4e508c0eea8da35541150ba80
injoon149 <dlswns8679@naver.com>
2022-07-17 18:15:48 +0900

    초기 커밋.

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+
+void Clock(int a, int b)
+{
+	int k;
+	if (b >= 60)
+	{
+		k = b / 60;
+		b = b % 60;
+		a += k;
+
+	}
+
+	if (a >= 24)
+	{
+		a %= 24;
+	}
+
+	cout << a <<" " << b << endl;
+}
+
+int main()
+{
+	int a, b;
+	cin >> a >> b;
+	int c = 0;
+	cin >> c;
+	Clock(a, b + c);
+
+	return 0;
+}
